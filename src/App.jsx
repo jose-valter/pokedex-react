e85de674de5efcc5d7dfcc08d51a352d8b628eea
José Valter <jvsjr@protonmail.com>
2022-11-14 18:30:07 -0300

    Search feature added

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,38 @@ function App() {
   const [pokemon, setPokemon] = React.useState([]);
   const [pokename, setPokename] = React.useState(null);
   const [pokephoto, setPokephoto] = React.useState(null);
+  const [search, setSearch] = React.useState('');
 
-  React.useEffect(() => {
-    async function fetchData(pokemon){
+
+  async function fetchData(pokemon){
+    try{
       const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
       const json = await response.json();
       console.log(json);
       setPokemon(json);
       setPokename(json.name);
       setPokephoto(json.sprites.versions['generation-v']['black-white'].animated['front_default'])
+    } catch(error){
+      alert('Ocorreu um erro. Por favor, procure por um Pokémon válido');
+      setSearch('');
     }
-    fetchData('pichu')
+  }
+  
+
+  function handleSubmit(event){
+    event.preventDefault();
+    const lowercaseSearch = search.toLowerCase();
+    fetchData(lowercaseSearch);
+    setSearch('');
+  }
+
+
+  React.useEffect(() => {
+    fetchData('1')
   }, [])
+
+
+
 
   return (
     <div className='App'>
@@ -29,8 +49,8 @@ function App() {
         <span className='pokeName'>{pokename}</span>
       </h1>
 
-      <form className='form' onSubmit={(e)=>{e.preventDefault()}}>
-        <input type="search" className='search' placeholder='Name or ID number' required />
+      <form className='form' onSubmit={handleSubmit}>
+        <input type="search" className='search' value={search} onChange={(e)=>{setSearch(e.target.value)}} placeholder='Name or ID number' required />
       </form>
 
       <div className='buttons'>
